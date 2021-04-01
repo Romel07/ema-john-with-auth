dab6a71c5d3e5981152fe3a2a82dadedaa8383f6
unknown <romel.barua@youngonectg.com>
2021-04-01 20:43:10 +0600

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCrXnsqBtAehvRq1Ge1nhKNlS2TEyeMa5s",
+  authDomain: "ema-john-270e0.firebaseapp.com",
+  projectId: "ema-john-270e0",
+  storageBucket: "ema-john-270e0.appspot.com",
+  messagingSenderId: "598660310329",
+  appId: "1:598660310329:web:5436d48322467b9ee734e8"
+};
+export default firebaseConfig;
